47f71e703a4e468946285afdf27854f438233bc9
Brett Koenig <brett9897@gmail.com>
2013-01-27 09:44:53 -0500

    updated admin_add

--- webroot/js/Questions/admin_add.js
+++ webroot/js/Questions/admin_add.js
@@ -44,8 +44,10 @@ $(document).ready(function (){
 			"Add validation": function()
 			{
 				$('#validation_text').append(
-					'<input type="hidden" id="validation_' + count + '" name="data[Question][validation_' + count + ']" value="' + $("#validation_id").val() + '" />' +
-					'<p><span class="text">' + $("#validation_id > option:selected").text() + '</span><span id="remove_image">' + count + '</span></p>'
+					'<div class="validation_entry">' + 
+						'<input type="hidden" id="validation_' + count + '" name="data[Question][validation_' + count + ']" value="' + $("#validation_id").val() + '" />' +
+						'<p><span class="text">' + $("#validation_id > option:selected").text() + '</span><span class="remove_image">' + count + '</span></p>' +
+					'</div>'
 				);
 				count++;
 				$( this ).dialog( 'close' );
@@ -69,6 +71,25 @@ $(document).ready(function (){
 	});
 
 	/* Validation remove */
-	$('#')
+	$('#validation_text').on('click', '.remove_image', function() {
+		var total = $('#validation_text .remove_image').length;
+
+
+		if( parseInt($(this).text()) != total )
+		{
+			//need to parse name attribute from remaining validations and subtract one
+			var current = $(this).parent().parent().next();
+			while( current.length > 0 )
+			{
+				alert(current.find('input[type=hidden]').attr('name'));
+				current = current.next();
+			}	
+		}
+		else
+		{
+			$(this).parent().parent().remove();
+			count--;
+		}
+	});
 
 });
