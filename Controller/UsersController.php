3ea1555cf16bee8184b7cf8a55b53a7201f88acf
GaTechDev <john@Duality.(none)>
2013-03-01 13:27:21 -0500

    re-committing the UsersController.php update for edit user.

--- Controller/UsersController.php
+++ Controller/UsersController.php
@@ -140,5 +140,64 @@ class UsersController extends AppController {
         $this->Session->setFlash(__('User was not deleted'));
         $this->redirect(array('action' => 'index'));
     }
+    
+    
+    /**
+     * edit method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function edit($id = null) {
+        $this->User->id = $id;
+        if (!$this->User->exists()) {
+            throw new NotFoundException(__('Invalid user'));
+        }
+        if ($this->request->is('post') || $this->request->is('put')) {
+            if ($this->User->save($this->request->data)) {
+                $this->Session->setFlash(__('The user has been saved'));
+                $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The user could not be saved. Please, try again.'));
+            }
+        } else {
+            $this->request->data = $this->User->read(null, $id);
+        }
+        $organizations = $this->User->Organization->find('list');
+        $this->set(compact('organizations'));
+    }
+    
+        /**
+     * change_password method
+     *
+     * @throws NotFoundException
+     * @param string $id if ()
+     * @return void
+     */
+    public function change_password($id = null) {
+        $this->User->id = $id;                                                  //How does it get the user id?....it is set null!
+        if (isset($this->request->data['cancel'])) {
+            $this->Session->setFlash(__('Password changes were cancelled.', true));
+            $this->redirect( array('action' => 'edit', $this->User->id) );  
+        }
+        else {
+
+            if (!$this->User->exists()) {
+                throw new NotFoundException(__('Invalid user'));
+            }
+            if ($this->request->is('post') || $this->request->is('put')) {                  //this checks to see if the form has been submitted
+                 $this->request->data['User']['pwd'] = $this->request->data['User']['password'];
+                 //$this->User->save($this->request->data);   
+                 if( $this->User->save($this->request->data) ){
+                    $this->Session->setFlash(__('Password has been Saved.'));
+                    $this->redirect( array('action' => 'edit', $this->User->id) );                 
+                 }
+            }else {
+                $this->request->data = $this->User->read(null, $id);
+            }   
+        }
+    }
 
 }
+
--- View/Users/admin_edit.ctp
+++ View/Users/admin_edit.ctp
@@ -14,7 +14,7 @@
 	<?php
 		echo $this->Form->input('id');
 		echo $this->Form->input('username');
-		echo $this->Form->input('password');
+		echo $this->Form->input('password');            //this is hidden
 		echo $this->Form->input('first_name');
 		echo $this->Form->input('last_name');
 		echo $this->Form->input('organization_id');
